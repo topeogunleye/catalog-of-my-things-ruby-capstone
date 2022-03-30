7c18da0db88c0e279519b2672bec2caf09545e62
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-30 18:26:03 +0100

    Add tests for genre and music_album classes

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,41 +1,27 @@
 # Test for genre.rb
 require_relative '../classes/genre'
+require_relative '../classes/items'
 
 describe Genre do
+  before :all do
+    @genre = Genre.new('Rock')
+  end
+
   it 'should be initialized with a name' do
-    genre = Genre.new('Rock')
-    expect(genre.name).to eq('Rock')
+    expect(@genre.name).to eq('Rock')
   end
 
   it 'should have an id' do
-    genre = Genre.new('Rock')
-    expect(genre.id).to be_a(Integer)
+    expect(@genre.id).to be_a(Integer)
   end
 
   it 'should have an array of items' do
-    genre = Genre.new('Rock')
-    expect(genre.items).to be_a(Array)
+    expect(@genre.items).to be_a(Array)
   end
 
   it 'should add items to the items array' do
-    genre = Genre.new('Rock')
-    item = Item.new('01/01/2000')
-    genre.add_item(item)
-    expect(genre.items).to include(item)
-  end
-
-  it 'should not add items that are not items to the items array' do
-    genre = Genre.new('Rock')
-    item = 'item'
-    genre.add_item(item)
-    expect(genre.items).not_to include(item)
-  end
-
-  it 'should not add items that are already in the items array' do
-    genre = Genre.new('Rock')
-    item = Item.new('01/01/2000')
-    genre.add_item(item)
-    genre.add_item(item)
-    expect(genre.items).to include(item)
+    item = Item.new('01-01-2000')
+    @genre.add_item(item)
+    expect(@genre.items).to include(item)
   end
 end
--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,23 +2,23 @@
 require_relative '../classes/music_album'
 
 describe MusicAlbum do
+  before :all do
+    @album = MusicAlbum.new('01-01-2000', false)
+  end
+
   it 'should be initialized with a publish date' do
-    album = MusicAlbum.new('01/01/2000', false)
-    expect(album.publish_date).to eq('01/01/2000')
+    expect(@album.publish_date).to eq('01-01-2000')
   end
 
   it 'should be initialized with a boolean for on_spotify' do
-    album = MusicAlbum.new('01/01/2000', false)
-    expect(album.on_spotify).to eq(false)
+    expect(@album.on_spotify).to eq(false)
   end
 
   it 'should have an id' do
-    album = MusicAlbum.new('01/01/2000', false)
-    expect(album.id).to be_a(Integer)
+    expect(@album.id).to be_a(Integer)
   end
 
   it 'should return true if parents method returns true and if on_spotify equals true' do
-    album = MusicAlbum.new('01/01/2000', true)
-    expect(album.on_spotify?).to eq(true)
+    expect(@album.can_be_archived?).to eq(false)
   end
 end
