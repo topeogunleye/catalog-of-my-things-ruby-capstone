af3e95a7ebb4bf739870c2953a40db9024cef451
blessedjasonmwanza <mwanzabj@gmail.com>
2022-03-31 09:48:26 +0200

    Refactor add author method

--- classes/author.rb
+++ classes/author.rb
@@ -3,7 +3,7 @@ class Author
   attr_reader :id, :items
 
   def initialize(first_name, last_name)
-    @id = Random.rand(1..1000)
+    @id = rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
