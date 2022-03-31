a7c823e117dd1913f571f0633df793b6eb801242
Afiz Savage <savageafiz9@gmail.com>
2022-03-31 11:10:46 +0000

    Added method to create json file

--- options/book_handler.rb
+++ options/book_handler.rb
@@ -3,10 +3,47 @@ require_relative '../classes/book'
 class BookHandler
   #   def initialize(file, data_to_save)
   #     @file_data = nil
-  #     @json_file = file
+  #     'books.json' = file
   #     @data_to_save = data_to_save
   #     @destructured_array = nil
   #   end
+
+  #   def load_file
+  #     file_to_load = File.read('books.json')
+  #     JSON.parse(file_to_load)
+  #   end
+
+  #   def files_to_load
+  #     load_files unless File.file?('books.json') == false
+  #   end
+
+  def create_file
+    new_file = File.new('books.json', 'w')
+    new_file.puts(JSON.pretty_generate([]))
+    new_file.close
+  end
+
+  def read_file_if_it_exists
+    create_file unless File.file?('books.json')
+    # @file_data = File.read('books.json')
+  end
+
+  #   def append_to_array
+  #     @destructured_array = JSON.parse(@file_data)
+
+  #     @to_save.each do |person|
+  #       @destructured_array << { class: person.class, id: person.id, name: person.name, age: person.age }
+  #     end
+  #   end
+
+  def preserve_data
+    read_file_if_it_exists
+    #   append_to_array
+
+    File.open('books.json', 'w') do |f|
+      f.puts JSON.pretty_generate(@destructured_array)
+    end
+  end
 
   def create_new_book
     print 'Author: '
@@ -26,46 +63,10 @@ class BookHandler
 
     book = Book.new(author, publisher, cover_state, label, publish_date)
 
+    read_file_if_it_exists
     # @newly_created_books << book
     # @books << book
     puts book
     # book = Book.new()
   end
-
-  #   def load_file
-  #     file_to_load = File.read(@json_file)
-  #     JSON.parse(file_to_load)
-  #   end
-
-  #   def files_to_load
-  #     load_files unless File.file?(@json_file) == false
-  #   end
-
-  #   def create_file
-  #     new_file = File.new(@json_file, 'w')
-  #     new_file.puts(JSON.pretty_generate([]))
-  #     new_file.close
-  #   end
-
-  #   def read_file_if_it_exists
-  #     create_file unless File.file?(@json_file)
-  #     @file_data = File.read(@json_file)
-  #   end
-
-  #   def append_to_array
-  #     @destructured_array = JSON.parse(@file_data)
-
-  #     @to_save.each do |person|
-  #       @destructured_array << { class: person.class, id: person.id, name: person.name, age: person.age }
-  #     end
-  #   end
-
-  #   def preserve_data
-  #     read_file_if_it_exists
-  #     append_to_array
-
-  #     File.open(@json_file, 'w') do |f|
-  #       f.puts JSON.pretty_generate(@destructured_array)
-  #     end
-  #   end
 end
